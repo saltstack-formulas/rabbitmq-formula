57efa458af19851ae030eb788f35fcf20bb157b6
Imran Iqbal <iqbalmy@hotmail.com>
2019-11-21 12:10:56 +0000

    test(rabbitmq_users_spec.rb): fix rubocop violations

--- test/integration/default/controls/rabbitmq_users_spec.rb
+++ test/integration/default/controls/rabbitmq_users_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-service_name = 'rabbitmq-server'
-
 control 'rabbitmq users' do
+  title 'should include/exclude the given users'
+
   describe command('rabbitmqctl list_users') do
-    its('stdout') { should match (/user1/) }
-    its('stdout') { should match (/user2/) }
-    its('stdout') { should_not match (/guest/) }
+    its('stdout') { should include 'user1' }
+    its('stdout') { should include 'user2' }
+    its('stdout') { should_not include 'guest' }
   end
 end
